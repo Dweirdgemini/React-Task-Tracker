faf79814483c9896d2f33082143651a1159bb951
Joshua Nwachuku <nwachukujosh2000@gmail.com>
2022-04-05 10:41:31 +0100

    master

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
+import Header from './components/Header';
 
 function App() {
 	
 	return (
-		<div className="container">
-			<h1>Hello From React</h1>
+		<div className="container">  
+			<Header />
 		</div>
 	)
 }
