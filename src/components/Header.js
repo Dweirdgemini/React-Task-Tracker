e3148ebfc6fa79156b948620184c39a3e8230974
Joshua Nwachuku <nwachukujosh2000@gmail.com>
2022-07-15 10:27:01 +0100

    m

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,10 @@
 const Header = () => {
   return (
-  
+  <nav className="flex fixed w-screen">
+    <div className="flex-2 justify-center mr-auto">
+    <a href="https://pluralsight.com" className="mx-3 ">Homet</a>
+    </div>
+  </nav>
   )
 }
 
