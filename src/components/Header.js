c2c735c69ed7e06276cd199c6d3b396337b76a57
Joshua Nwachuku <nwachukujosh2000@gmail.com>
2022-07-15 10:47:30 +0100

    master

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,11 @@
 const Header = () => {
   return (
   <nav className="flex fixed w-screen">
-    <div className="flex-2 justify-center mr-auto">
-    <a href="https://pluralsight.com" className="mx-3 ">Homet</a>
+    <div className="flex-2 justify-center mr-auto mx-3 space-x-4 ">
+    <a href="https://pluralsight.com" className="font-DancingScript ">Home</a>
+    <a href="https://pluralsight.com" className="font-DancingScript ">Services</a>
     </div>
+    <div></div>
   </nav>
   )
 }
